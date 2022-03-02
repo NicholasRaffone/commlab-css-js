e04ad869be593dcdade91161946ef42f1a543471
Nicholas Raffone <nicholas.raffone@gmail.com>
2022-03-02 18:16:51 +0400

    adding sound

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const soundbutton = document.getElementById('soundbutton')
 var rellax = new Rellax('.rellax');
+var sound = new Howl({
+    src: ['https://assets.mixkit.co/sfx/preview/mixkit-fast-small-sweep-transition-166.mp3'],
+  });
+
+
+soundbutton.addEventListener("click", () => {sound.play(); playing=false;})
+
